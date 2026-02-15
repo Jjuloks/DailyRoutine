1480bf2958e9b290ae056c3ce6a925a318cb6b21
Julian <julox8062@gmail.com>
2026-02-15 16:10:18 +0100

    add Task API with Django REST Framework

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+
 
 # Create your views here.
