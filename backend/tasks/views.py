d30d3a58dcac9c334d4e7542c6eacb99dc78c346
Julian <julox8062@gmail.com>
2026-02-16 19:55:32 +0100

    add tommorrow task endpoint

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Task
 from .serializers import TaskSerializer
 from django.utils import timezone
-from datetime import date
+from datetime import date, datetime,  timedelta
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -19,6 +19,14 @@ class TaskViewSet(viewsets.ModelViewSet):
     def today(self, request):
         today_date = date.today()
         queryset = Task.objects.filter(deadline = today_date)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=["get"])
+    def tommorow(self, request):
+        today_date = date.today()
+        tommorrow_date =  today_date + timedelta(days=1)
+        queryset = Task.objects.filter(deadline = tommorrow_date)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
