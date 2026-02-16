74f06c074f3828039424dc99aaa54c3fb82e2d00
Julian <julox8062@gmail.com>
2026-02-16 19:21:20 +0100

    auto set completed_at when done

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,11 +1,22 @@
 from rest_framework import serializers
 from .models import Task
 
-
+from django.utils import timezone
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = "__all__"
 
+
+    def update(self, instance, validated_data):
+        old_status = instance.status
+        new_status= validated_data.get("status", old_status)
+
+
+        if old_status !="done" and new_status == "done":
+            validated_data["completed_at"] = timezone.now()
+        
+        return super().update(instance, validated_data)
+  
 
 
--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from .models import Task
 from rest_framework import viewsets
 from .serializers import TaskSerializer
+from django.utils import timezone
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
+    
     serializer_class = TaskSerializer
+
 
 
 
