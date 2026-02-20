f03511e69e48b6ecc66bc194bfe91be9a1d406e3
Julian <julox8062@gmail.com>
2026-02-20 13:28:35 +0100

    Layout for Contact(LeftSide -Personal Info))

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -4,25 +4,38 @@ import { FiPhone, FiMail, FiUser } from "react-icons/fi";
 const ContactPage = () => {
   return (
     <div className="page-container">
+
       <h1 className="contact-title">Contact Us</h1>
 
       <div className="contact-layout">
-        <div className="contact-info">
-          <div className="my-info">
-            <h2>Julian Oksimowicz</h2>
-            <p>
-              Phone:{" "}
-              <a href="tel:+48796429229">+48 796 429 229</a>
-            </p>
-            <p>
-              <a href="mailto:julowicz@gmail.com">
-                julowicz@gmail.com
-              </a>
-            </p>
 
-            <p className="contact-description">
-           Contact Me
+        <div className="contact-info">
+
+          <div className="my-info">
+
+            <div className="info-top">
+            <h1>Chasing IT</h1>
+             </div>
+
+            <div className="info-middle">
+            <h2>Julian Oksimowicz</h2>
+            <div className="info-item"> 
+            <FiPhone className="icon" />
+              <a href="tel:+48796429229">+48 796 429 229</a>
+            </div>
+
+            <div classname="info-item">
+            <FiMail className="icon" />
+              <a href="mailto:julowicz@gmail.com"> julowicz@gmail.com</a>
+            </div>
+
+            </div>
+            
+            <div className="info-bottom"> 
+            <p className="info-shorttext">
+            Feel free to contact me. Im open to new ideas.
             </p>
+          </div>
           </div>
         </div>
 
